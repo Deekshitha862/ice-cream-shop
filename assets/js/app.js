5579b620889280ba2586ea7152242406e8c301db
Deekshitha862 <deeshe2588@gmail.com>
2024-09-15 20:15:20 +0530

    static page is done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,51 @@
-const productElement=document.getElementById('product');
-productElement.addEventListener('click',function(){
-    window.location.href='product.html';
-});
+const products=[
+    {
+        id:1
+        name: 'Ice cream 1',
+        price:10,
+        quantity:50,
+        description:This is ice cream 1,
+        image:"https://placehold.co/350x350/blue/white?text=Ice+cream+1"
+    },
+    {
+        id:2
+        name: 'Ice cream 2',
+        price:20,
+        quantity:55,
+        description:This is ice cream 2,
+        image:"https://placehold.co/350x350/blue/white?text=Ice+cream+2"
+    },
+    {
+        id:3
+        name: 'Ice cream 3',
+        price:60,
+        quantity:80,
+        description:This is ice cream 3,
+         image:"https://placehold.co/350x350/blue/white?text=Ice+cream+3"
+    },
+  {
+    id:4
+    name: 'Ice cream 4',
+    price:60,
+    quantity:80,
+    description:This is ice cream 3,
+     image:"https://placehold.co/350x350/blue/white?text=Ice+cream+4"
+  },
+  {
+    id:5
+    name: 'Ice cream 5',
+    price:60,
+    quantity:80,
+    description:This is ice cream 3,
+     image:"https://placehold.co/350x350/blue/white?text=Ice+cream+5"
+  },
+  {
+    id:6
+    name: 'Ice cream 6',
+    price:60,
+    quantity:80,
+    description:This is ice cream 3,
+     image:"https://placehold.co/350x350/blue/white?text=Ice+cream+6"
+
+  }
+];
